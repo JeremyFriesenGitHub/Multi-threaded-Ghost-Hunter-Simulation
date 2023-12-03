cdc16ed941c4ed11e3c3bd7db678a07c6df18ca3
Jeremy Friesen <123791412+JeremyFriesenGitHub@users.noreply.github.com>
2023-12-02 23:29:01 -0500

    Add files via upload

--- defs.h
+++ defs.h
@@ -8,15 +8,16 @@
 
 #define MAX_STR         64
 #define MAX_RUNS        50
-#define BOREDOM_MAX     100
+#define BOREDOM_MAX     50
 #define C_TRUE          1
 #define C_FALSE         0
 #define HUNTER_WAIT     3500
 #define GHOST_WAIT      1500
 #define NUM_HUNTERS     4
-#define FEAR_MAX        10      
+#define FEAR_MAX        25
 #define LOGGING         C_TRUE
-#define MAX_EVIDENCE    100
+#define MAX_EVIDENCE    4
+
 
 typedef enum EvidenceType EvidenceType;
 typedef struct EvidenceNode EvidenceNode;
